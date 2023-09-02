f8be39cc10f23959968a7d7cfa117a9416cd5d6f
Vincenzo-Lauro <laurovincenzo.097@gmail.com>
2023-09-02 15:01:23 +0200

    First implementation of solve and reset buttons.

--- WaveFunctionCollapse/main.cpp
+++ WaveFunctionCollapse/main.cpp
@@ -1,19 +1,44 @@
 #include "mainwindow.h"
+#include "DataStructures.h"
+
 #include <iostream>
+#include <memory>
 
 #include <QApplication>
-#include <QLayout>
+#include <QPushButton>
 
 #include "wfctile.h"
+
+static constexpr Vector2D BUTTON_DEFAULT_SIZE{100,30};
+static constexpr Vector2D SOLVE_BUTTON_OFFSET{250,50};
+static constexpr Vector2D RESET_BUTTON_OFFSET{125,50};
+
+void InitializeMainWindow(MainWindow* mainWindow);
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     MainWindow w;
 
-    const auto tile = std::make_unique<WFCTile>(100,100,"/home/vincenzo/Documents/C++/WaveFunctionCollapse/WaveFunctionCollapse/chopper.png");
-    w.layout()->addWidget(tile->GetPresenter().get());
+    InitializeMainWindow(&w);
+
+    //const auto tile = std::make_unique<WFCTile>(100,100,"/home/vincenzo/Documents/C++/WaveFunctionCollapse/WaveFunctionCollapse/chopper.png");
+    //w.layout()->addWidget(tile->GetPresenter().get());
 
     w.show();
     return app.exec();
 }
+
+void InitializeMainWindow(MainWindow* mainWindow){
+    auto resetButton = std::make_unique<QPushButton>("Reset");
+    auto solveButton = std::make_unique<QPushButton>("Solve");
+
+    resetButton->setFixedSize(BUTTON_DEFAULT_SIZE.X,BUTTON_DEFAULT_SIZE.Y);
+    solveButton->setFixedSize(BUTTON_DEFAULT_SIZE.X,BUTTON_DEFAULT_SIZE.Y);
+
+    const Vector2D windowSize{mainWindow->size().width(),mainWindow->size().height()};
+
+    mainWindow->AddWidget(std::move(resetButton), BUTTON_DEFAULT_SIZE, windowSize-RESET_BUTTON_OFFSET);
+    mainWindow->AddWidget(std::move(solveButton), BUTTON_DEFAULT_SIZE, windowSize-SOLVE_BUTTON_OFFSET);
+}
+
--- WaveFunctionCollapse/mainwindow.cpp
+++ WaveFunctionCollapse/mainwindow.cpp
@@ -1,15 +1,29 @@
 #include "mainwindow.h"
+#include <QLayout>
+#include <QDesktopWidget>
 #include "./ui_mainwindow.h"
+#include "DataStructures.h"
+
+static constexpr float APP_RELATIVE_SIZE{0.5};
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    setFixedSize(QDesktopWidget().availableGeometry(this).size() * APP_RELATIVE_SIZE);
     ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::AddWidget(std::unique_ptr<QWidget> widgetToAdd, const Vector2D& size ,const Vector2D& position){
+    widgetToAdd->setGeometry(position.X,position.Y, size.X, size.Y);
+    storedWidgets.push_back(std::shared_ptr<QWidget>(std::move(widgetToAdd)));
+
+    layout()->addWidget(storedWidgets.back().get());
+
 }
 
