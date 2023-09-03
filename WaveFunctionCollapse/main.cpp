feb57c94c98cfc00e8162b19a73995a96ada0c53
Vincenzo-Lauro <laurovincenzo.097@gmail.com>
2023-09-03 19:37:44 +0200

    Buttons spawned + refactor memory management according to Qt UI guidelines

--- WaveFunctionCollapse/main.cpp
+++ WaveFunctionCollapse/main.cpp
@@ -7,14 +7,18 @@
 #include <QApplication>
 #include <QPushButton>
 #include <QLabel>
+#include <QGridLayout>
 
 #include "wfctile.h"
 
 static constexpr Vector2D BUTTON_DEFAULT_SIZE{100,30};
 static constexpr Vector2D SOLVE_BUTTON_OFFSET{250,50};
 static constexpr Vector2D RESET_BUTTON_OFFSET{125,50};
+static constexpr int ROWS_NUM{6};
+static constexpr int COLUMNS_NUM{6};
 
 void InitializeMainWindow(MainWindow* mainWindow);
+QLabel* CreateMap();
 
 int main(int argc, char *argv[])
 {
@@ -27,20 +31,41 @@ int main(int argc, char *argv[])
     return app.exec();
 }
 
+QLabel* CreateMap(){
+    auto mapLabel = new QLabel();
+    mapLabel->setStyleSheet("QLabel { background-color : Ivory; border: 3px solid black;}");
+    auto gridLayout = new QGridLayout(mapLabel);
+    gridLayout->setHorizontalSpacing(0);
+    gridLayout->setVerticalSpacing(0);
+    const auto layoutGeometry = mapLabel->geometry();
+
+    for(int rowIndex=0; rowIndex<ROWS_NUM; rowIndex++){
+        for(int columnIndex=0; columnIndex<COLUMNS_NUM; columnIndex++){
+            auto tileButton = new QPushButton("");
+            const auto width = layoutGeometry.width()/COLUMNS_NUM;
+            const auto height = layoutGeometry.height()/ROWS_NUM;
+            tileButton->setFixedSize(width,height);
+            gridLayout->addWidget(tileButton, columnIndex, rowIndex);
+        }
+    }
+
+
+    return mapLabel;
+}
+
 void InitializeMainWindow(MainWindow* mainWindow){
-    auto resetButton = std::make_unique<QPushButton>("Reset");
-    auto solveButton = std::make_unique<QPushButton>("Solve");
-    auto mainLabel = std::make_unique<QLabel>();
-    mainLabel->setStyleSheet("QLabel { background-color : Ivory; border: 3px solid black;}");
+    auto resetButton = new QPushButton("Reset");
+    auto solveButton = new QPushButton("Solve");
+
+    auto mapLabel = CreateMap();
 
     resetButton->setFixedSize(BUTTON_DEFAULT_SIZE.X,BUTTON_DEFAULT_SIZE.Y);
     solveButton->setFixedSize(BUTTON_DEFAULT_SIZE.X,BUTTON_DEFAULT_SIZE.Y);
 
     const Vector2D windowSize{mainWindow->size().width(),mainWindow->size().height()};
-
     const Vector2D screenSize = {windowSize.X - 20, windowSize.Y - 2 * BUTTON_DEFAULT_SIZE.Y - 2};
-    mainWindow->AddWidget(std::move(mainLabel), screenSize, {10,5});
-    mainWindow->AddWidget(std::move(resetButton), BUTTON_DEFAULT_SIZE, windowSize-RESET_BUTTON_OFFSET);
-    mainWindow->AddWidget(std::move(solveButton), BUTTON_DEFAULT_SIZE, windowSize-SOLVE_BUTTON_OFFSET);
+    mainWindow->AddWidget(mapLabel, screenSize, {10,5});
+    mainWindow->AddWidget(resetButton, BUTTON_DEFAULT_SIZE, windowSize-RESET_BUTTON_OFFSET);
+    mainWindow->AddWidget(solveButton, BUTTON_DEFAULT_SIZE, windowSize-SOLVE_BUTTON_OFFSET);
 }
 
--- WaveFunctionCollapse/mainwindow.cpp
+++ WaveFunctionCollapse/mainwindow.cpp
@@ -4,13 +4,14 @@
 #include "./ui_mainwindow.h"
 #include "DataStructures.h"
 
-static constexpr float APP_RELATIVE_SIZE{0.5};
+static constexpr float APP_RELATIVE_WIDTH{0.888};
+static constexpr float APP_RELATIVE_HEIGHT{0.5};
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
-    setFixedSize(QDesktopWidget().availableGeometry(this).size() * APP_RELATIVE_SIZE);
+    setFixedSize(QDesktopWidget().availableGeometry(this).width(),QDesktopWidget().availableGeometry(this).height());
     ui->setupUi(this);
 }
 
@@ -19,10 +20,8 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::AddWidget(std::unique_ptr<QWidget> widgetToAdd, const Vector2D& size ,const Vector2D& position){
+void MainWindow::AddWidget(QWidget* widgetToAdd, const Vector2D& size ,const Vector2D& position){
     widgetToAdd->setGeometry(position.X,position.Y, size.X, size.Y);
-    storedWidgets.push_back(std::shared_ptr<QWidget>(std::move(widgetToAdd)));
-
-    layout()->addWidget(storedWidgets.back().get());
+    layout()->addWidget(widgetToAdd);
 }
 
