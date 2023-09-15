b47ad6a08634e3c60c34a39bc290af3786a8782a
Lauro-Vincenzo <87123799+Lauro-Vincenzo@users.noreply.github.com>
2023-09-15 18:23:42 +0200

    5 apply an image to the tiles (#8)
    * First implementation
    
    * Minor changes
    
    * First round of redesign
    
    * Applied suggested changes

---
+++ WaveFunctionCollapse/IMapManager.h
@@ -0,0 +1,11 @@
+#ifndef IMAPMANAGER_H
+#define IMAPMANAGER_H
+
+
+class IMapManager
+{
+public:
+    virtual void InitializeMap() = 0;
+};
+
+#endif // IMAPMANAGER_H
--- WaveFunctionCollapse/main.cpp
+++ WaveFunctionCollapse/main.cpp
@@ -1,78 +1,17 @@
-#include "mainwindow.h"
-#include "DataStructures.h"
-
-#include <iostream>
 #include <memory>
 
 #include <QApplication>
-#include <QPushButton>
-#include <QLabel>
-#include <QGridLayout>
 
-#include "wfctile.h"
-
-static constexpr Vector2D BUTTON_CONTAINER_POSITION{250,50};
-static constexpr Vector2D TOPLEFT_STARTPOINT{0,0};
-
-static constexpr double MAP_WIDTH_FILL_FACTOR{0.70};
-static constexpr int ROWS_NUM{8};
-static constexpr int COLUMNS_NUM{8};
-
-void InitializeMainWindow(MainWindow* mainWindow);
-QLabel* CreateMap();
+#include "MapManagerImpl.h"
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    MainWindow w;
 
-    InitializeMainWindow(&w);
+    std::unique_ptr<IMapManager> mapManager = std::make_unique<MapManagerImpl>();
+    mapManager->InitializeMap();
 
-    w.show();
     return app.exec();
 }
 
-QLabel* CreateMap(){
-    auto mapLabel = new QLabel();
-    mapLabel->setStyleSheet("QLabel { background-color : Ivory; border: 3px solid black;}");
-    auto gridLayout = new QGridLayout(mapLabel);
-    gridLayout->setGeometry(QRect(0,0,mapLabel->width(), mapLabel->height()));
-
-    gridLayout->setHorizontalSpacing(0);
-    gridLayout->setVerticalSpacing(0);
-
-    gridLayout->setContentsMargins(0,0,0,0);
-
-    for(int rowIndex=0; rowIndex<ROWS_NUM; rowIndex++){
-        for(int columnIndex=0; columnIndex<COLUMNS_NUM; columnIndex++){
-            auto tileButton = new QPushButton("");
-            tileButton->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-            gridLayout->addWidget(tileButton, columnIndex, rowIndex);
-        }
-    }
-
-    return mapLabel;
-}
-
-void InitializeMainWindow(MainWindow* mainWindow){
-    const auto& mapLabel = CreateMap();
-
-    const auto& buttonContainerWidget = new QWidget();
-    const auto& buttonVBox = new QVBoxLayout(buttonContainerWidget);
-
-    const auto& resetButton = new QPushButton("Reset");
-    const auto& solveButton = new QPushButton("Solve");
-    buttonVBox->addWidget(resetButton);
-    buttonVBox->addWidget(solveButton);
-
-    const int& squaredMapSide = mainWindow->width()*MAP_WIDTH_FILL_FACTOR;
-
-    const Vector2D screenSize = {squaredMapSide,squaredMapSide};
-
-    const Vector2D buttonSize = {int(squaredMapSide*0.25),squaredMapSide};
-    const Vector2D buttonPosition = {squaredMapSide,0};
-
-    mainWindow->AddWidget(mapLabel, screenSize, TOPLEFT_STARTPOINT);
-    mainWindow->AddWidget(buttonContainerWidget, buttonSize, buttonPosition);
-}
 
